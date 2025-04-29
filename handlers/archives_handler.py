b616dd6b0cd2de112ec97fa02a716caf48b0039a
Aleksey Salamatin <asalamatin640@gmail.com>
2025-04-29 20:57:41 +0500

    processing zip, tar, 7z

--- handlers/archives_handler.py
+++ handlers/archives_handler.py
@@ -1,8 +1,10 @@
-from aiogram import Bot, Router, types
-from aiogram.filters import Command
+from aiogram import Bot, Router
 from aiogram.types import Message
+from aiogram.enums import ParseMode
 from aiogram import F
-import os
+
+from processing_responses.processing_archives import processing_archive
+from processing_responses.processing_transcribation import processing_transcribation
 
 router = Router()
 
@@ -17,3 +19,18 @@ ALLOWED_ARCHIVES = {'zip', 'rar', '7z', 'tar', 'gz', 'bz2'}
 async def get_archive(message: Message, bot: Bot):
     print("get_archive")
     archive = message.document
+    if archive:
+        arc_name = archive.file_name
+        arc_id = archive.file_id
+        arc_type = archive.mime_type
+        print(arc_type)
+    if arc_name and arc_type:
+        transcribation = await processing_archive(bot, arc_name, arc_id, arc_type)
+        print(transcribation)
+        analize = await processing_transcribation(transcribation)
+        print("resp = " + analize)
+
+    await message.answer(
+        f"Транскрибация аудио: \n{transcribation}\n\nАнализ: \n{analize}",
+        parse_mode=ParseMode.MARKDOWN
+    )
---
+++ processing_responses/processing_archives.py
@@ -0,0 +1,89 @@
+from aiogram import Bot
+import zipfile, tarfile, py7zr
+
+from my_requests.request_for_get_long_ogg_tr import get_long_ogg_tr
+from my_requests.request_to_post_file_on_bucket import post_file
+from processing_responses.converting_audio import convert_to_ogg
+
+async def processing_archive(bot: Bot, arc_name: str, file_id: str, arc_type: str) -> str:
+    processed_arc_type = await get_arc_type(arc_type)
+    if processed_arc_type:
+        file_name = await extract_arc(bot, arc_name, file_id, processed_arc_type)
+
+    type = file_name.split(".")[1]
+    if type == "ogg":
+        post_file(file_name)
+        transcribation = get_long_ogg_tr(file_name)
+    elif type == "mp3":
+        file_name = convert_to_ogg(file_name, "audio/mpeg")
+        post_file(file_name)
+        transcribation = get_long_ogg_tr(file_name)
+    elif type == "wav" or "WAV":
+        file_name = convert_to_ogg(file_name, "audio/vnd.wave")
+        post_file(file_name)
+        transcribation = get_long_ogg_tr(file_name)
+    else:
+        pass
+
+    return transcribation
+
+async def extract_arc(bot: Bot, arc_name: str, file_id: str, arc_type: str) -> str:
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    dst = f"downloads/{arc_name}"
+    if file_path:
+        await bot.download_file(file_path, dst)
+        print("download_arc")
+
+    if arc_type == "zip":
+        with zipfile.ZipFile(f'downloads/{arc_name}', 'r') as zip_ref:
+            files_list = zip_ref.namelist()
+            if len(files_list) > 1:
+                #вывести сообщение о том что много файлов в апхиве
+                pass
+            elif len(files_list) == 0:
+                #вывести сообщение что нет файлов в архиве
+                pass
+            else:
+                file_name = files_list[0]
+
+            zip_ref.extractall('downloads/')
+            print("extract")
+    elif arc_type == "tar":
+        with tarfile.open(f'downloads/{arc_name}', 'r:*') as tar:
+            files_list = tar.getnames()
+            if len(files_list) > 1:
+                #вывести сообщение о том что много файлов в апхиве
+                pass
+            elif len(files_list) == 0:
+                #вывести сообщение что нет файлов в архиве
+                pass
+            else:
+                file_name = files_list[0]
+
+            tar.extractall('downloads/')
+    elif arc_type == "7z":
+        with py7zr.SevenZipFile(f'downloads/{arc_name}', mode='r') as z:
+            files_list = z.getnames()
+            if len(files_list) > 1:
+                #вывести сообщение о том что много файлов в апхиве
+                pass
+            elif len(files_list) == 0:
+                #вывести сообщение что нет файлов в архиве
+                pass
+            else:
+                file_name = files_list[0]
+
+            z.extractall('downloads/')
+
+    return file_name
+
+async def get_arc_type(type: str) -> str | None:
+    if type == "application/x-tar":
+        return "tar"
+    elif type == "application/zip":
+        return "zip"
+    elif type == "application/x-7z-compressed":
+        return "7z"
+    else:
+        return None
